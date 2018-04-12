20f0e98d45e666a698aa824919d1377fd19275df
Christian Aebloe <chraebe@live.dk>
2018-04-12 10:47:17 +0200

    Test123

---
+++ server/src/main/java/sw806f18/server/api/GroupResource.java
@@ -0,0 +1,107 @@
+package sw806f18.server.api;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import sw806f18.server.Authentication;
+import sw806f18.server.database.Database;
+import sw806f18.server.exceptions.HubException;
+import sw806f18.server.exceptions.P8Exception;
+import sw806f18.server.exceptions.SurveyException;
+import sw806f18.server.model.Hub;
+import sw806f18.server.model.Survey;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("group/{id}")
+public class GroupResource {
+    /**
+     * Get Surveys.
+     *
+     * @param token Token.
+     * @return Surveys Metadata
+     */
+    @GET
+    @Path("/surveys")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getSurveys(@PathParam("id") int id,
+                             @HeaderParam("token") String token) {
+        if (Database.isResearcher(Authentication.instance.getId(token))) {
+            List<Survey> modules;
+
+            try {
+                modules = Database.getGroupLinks(id);
+            } catch (SurveyException e) {
+                return "";
+            }
+
+            String jsonGroup = "{ \"modules\": [ ";
+            for (int i = 0; i < modules.size(); i++) {
+                if (i == 0) {
+                    jsonGroup += modules.get(i).getJsonObject();
+                } else {
+                    jsonGroup += ", " + modules.get(i).getJsonObject();
+                }
+            }
+            jsonGroup += "]}";
+            return jsonGroup;
+        }
+
+        return "";
+    }
+
+    /**
+     * Delete link
+     *
+     * @param token    Token.
+     * @param id       ID.
+     * @param moduleId module
+     * @return Surveys Metadata
+     */
+    @POST
+    @Path("/link/delete")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String deleteLink(@PathParam("id") int id,
+                             @HeaderParam("module") int moduleId,
+                             @HeaderParam("token") String token) {
+        if (Database.isResearcher(Authentication.instance.getId(token))) {
+            try {
+                Database.removeGroupLink(id, moduleId);
+                return "{\"200\": \"success\"}";
+            } catch (SurveyException e) {
+                return "{\"error\": \"No such link\"}";
+            }
+        }
+
+        return "{\"error\": \"Invalid token\"}";
+    }
+
+    /**
+     * Linking a group to a survey.
+     * @param id
+     * @param moduleId
+     * @param token
+     * @return
+     */
+    @PUT
+    @Path("/link/add")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject linkSurveyToGroup(@PathParam("id") int id,
+                                        @HeaderParam("module") int moduleId,
+                                        @HeaderParam("token") String token) {
+        if (Database.isResearcher(Authentication.instance.getId(token))) {
+            try {
+                Database.linkModuleToGroup(moduleId, id);
+            } catch (SurveyException e) {
+                return Json.createObjectBuilder().add("error", e.getMessage()).build();
+            } catch (P8Exception e) {
+                e.printStackTrace();
+            }
+
+            return Json.createObjectBuilder().add("success", 1).build();
+        }
+        return Json.createObjectBuilder().add("error", "Invalid token").build();
+    }
+}
---
+++ server/src/test/java/sw806f18/server/api/GroupResourceTest.java
@@ -0,0 +1,81 @@
+package sw806f18.server.api;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import sw806f18.server.TestHelpers;
+import sw806f18.server.TestListener;
+import sw806f18.server.TestRunner;
+import sw806f18.server.database.Database;
+import sw806f18.server.exceptions.P8Exception;
+import sw806f18.server.exceptions.SurveyException;
+import sw806f18.server.model.Survey;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(TestRunner.class)
+public class GroupResourceTest {
+    @Test
+    public void getGroupLinks() {
+        Response response = TestHelpers.getWithToken(TestListener.target,
+                TestHelpers.GROUP_PATH + "/" + TestHelpers.group1.getId() + "/surveys",
+                TestHelpers.tokenResearcher1);
+        assertEquals(200, response.getStatus());
+        JsonObject object = TestHelpers.getPayload(response);
+        JsonArray array = object.getJsonArray("modules");
+        List<Survey> modules = new ArrayList<>();
+        for(int i = 0; i < array.size(); i++){
+            JsonObject element = array.get(i).asJsonObject();
+            modules.add(new Survey(element.getInt("id"), element.getString("title"), element.getString("description")));
+        }
+
+        List<Survey> expected = new ArrayList<>();
+        expected.add(TestHelpers.survey1);
+        expected.add(TestHelpers.survey2);
+
+        for (int i = 0; i < modules.size(); i++) {
+            assertEquals(modules.get(i).getId(), expected.get(i).getId());
+            assertTrue(modules.get(i).getTitle().equals(expected.get(i).getTitle()));
+            assertTrue(modules.get(i).getDescription().equals(expected.get(i).getDescription()));
+        }
+    }
+
+    @Test
+    public void removeGroupLink() {
+        Response response = TestHelpers.removeGroupLink(TestListener.target,
+                TestHelpers.GROUP_PATH + "/" + TestHelpers.group1.getId() + "/link/delete",
+                TestHelpers.survey2.getId(),
+                TestHelpers.tokenResearcher1);
+        assertEquals(200, response.getStatus());
+
+        boolean hasError = false;
+        List<Survey> modules = null;
+        try {
+            modules = Database.getGroupLinks(TestHelpers.group1.getId());
+        } catch (SurveyException e) {
+            hasError = true;
+        }
+        assertFalse(hasError);
+        assertEquals(1, modules.size());
+        assertEquals(modules.get(0).getId(), TestHelpers.survey1.getId());
+    }
+
+    @Test
+    public void linkModuleToGroup() throws P8Exception, SQLException, ClassNotFoundException {
+        Response response = TestHelpers.linkModuleToSurvey(TestListener.target,
+                TestHelpers.GROUP_PATH + "/" + TestHelpers.group1.getId() + "/link/add",
+                TestHelpers.survey1.getId(), TestHelpers.tokenResearcher1);
+        assertEquals(200, response.getStatus());
+
+        List<Integer> linkedGroups = Database.getModuleLinks(TestHelpers.survey1);
+        assertTrue(linkedGroups.contains(TestHelpers.group1.getId()));
+    }
+}
