743f79aff24ad0d16bc7789899001f29b01d5f76
Unknown <lille.bob@hotmail.com>
2018-05-09 11:37:38 +0200

    Fix merge and style a bit

--- server/src/main/resources/static/webportal/js/sidepanel.js
+++ server/src/main/resources/static/webportal/js/sidepanel.js
@@ -1,10 +1,11 @@
 function sidepanel() {
     //var content = document.body.innerHTML;
     document.body.innerHTML = "<div class=\"sidenav\">\n" +
-        "    <a class='sidepanela' href='../groupmanager/index.html' onclick='validateLogin()'>Group Manager</a>\n" +
-        "    <a class='sidepanela' href='../adduser/index.html' onclick='validateLogin()'>Create User</a>\n" +
-        "    <a class='sidepanela' href='../surveymanager/index.html' onclick='validateLogin()'>Create Survey</a>\n" +
-        "    <a class='sidepanela' href='../login/index.html' onclick='logout()'>Log Out</a>\n" +
+        "    <a class='sidepanela' href='../groupmanager/index.html' onclick='validateLogin()'>Gruppe manager</a>\n" +
+        "    <a class='sidepanela' href='../adduser/index.html' onclick='validateLogin()'>Opret bruger</a>\n" +
+        "    <a class='sidepanela' href='../surveymanager/index.html' onclick='validateLogin()'>Opret spørgskema</a>\n" +
+        "    <a class='sidepanela' href='../QueryPage/index.html' onclick='validateLogin()'>Exporter data</a> \n " +
+        "    <a class='sidepanela' href='../login/index.html' onclick='logout()'>Log ud</a>\n" +
         "</div>";
         // "<div class=\"main\">\n" +
         // content + "\n" +
